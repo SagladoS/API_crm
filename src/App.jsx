12e10cda6914faef5d70093a708b9f708d09d8d1
Saglado <ssalgadod@correo.iue.edu.co>
2022-05-31 07:50:34 -0500

    finily

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,21 @@ import VerCliente from './paginas/VerCliente'
 
 function App() {
   
+  
 
-  return (
-    
-    <BrowserRouter>
-      <Routes>
-          <Route path='/clientes' element={<Layout />} >
-            <Route index element={<Inicio />} />
-            <Route path="nuevo" element={<NuevoCliente />} />
-            <Route path="editar/:id" element={<EditarCliente />} />
-            <Route path=":id" element={<VerCliente />} />
-          </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+      
+      <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Layout />} >
+              <Route index element={<Inicio />} />
+              <Route path="nuevo" element={<NuevoCliente />} />
+              <Route path="editar/:id" element={<EditarCliente />} />
+              <Route path=":id" element={<VerCliente />} />
+            </Route>
+        </Routes>
+      </BrowserRouter>
+    )
 }
 
 export default App
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -21,7 +21,7 @@ const Formulario = ({ cliente, cargando }) => {
             let respuesta
             if (cliente.id) {
                 //ediar cliente
-                const url = `http://localhost:4000/clientes/${cliente.id}`
+                const url = `${import.meta.env.VITE_API_URL}/${cliente.id}`
                 respuesta = await fetch(url, {
                     method: 'PUT',
                     body: JSON.stringify(valores),
@@ -32,7 +32,7 @@ const Formulario = ({ cliente, cargando }) => {
 
             } else {
                 //nuevo registro
-                const url = 'http://localhost:4000/clientes'
+                const url = `${import.meta.env.VITE_API_URL}/clientes`
                 respuesta = await fetch(url, {
                     method: 'POST',
                     body: JSON.stringify(valores),
